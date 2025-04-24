b1199ce2bbc169dd6d92d74135bc2afe624099cf
Syeda Noorain <syeda.23bcs10221@ms.sst.scaler.com>
2025-04-24 22:46:28 +0530

    :)

--- src/app/notes/edit/page.tsx
+++ src/app/notes/edit/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter} from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
 
 export default function EditNote() {
